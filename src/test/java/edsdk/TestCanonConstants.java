4a2d3652074a3e0a9ad57f59c7038026ce4b6dd9
Wolfgang Fahl <wf@bitplan.com>
2016-08-20 18:52:45 +0200

    adds test for CanonConstants

---
+++ src/test/java/edsdk/TestCanonConstants.java
@@ -0,0 +1,25 @@
+package edsdk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edsdk.utils.CanonConstants;
+import edsdk.utils.CanonConstants.EdsAEMode;
+
+/**
+ * check that all constants are ok
+ * @author wf
+ *
+ */
+public class TestCanonConstants {
+
+  @Test
+  public void testConstants() {
+    boolean result = CanonConstants.verifyAllConstants();
+    assertTrue("Verify of CanonConstants should be ok",result);
+    EdsAEMode[] aemodes = CanonConstants.EdsAEMode.values();
+    assertEquals(20,aemodes.length);
+  }
+
+}

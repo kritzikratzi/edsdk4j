081f80e436a8eb43a540915db8226af86690cb79
Carsten Hammer <carsten.hammer@t-online.de>
2016-10-06 12:48:45 +0200

    use jna to compute library version

--- src/test/java/edsdk/TestLibrary.java
+++ src/test/java/edsdk/TestLibrary.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
+import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.profesorfalken.jpowershell.PowerShell;
 import com.profesorfalken.jpowershell.PowerShellResponse;
+import com.sun.jna.Memory;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.VerRsrc.VS_FIXEDFILEINFO;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.PointerByReference;
 
 import edsdk.CmdLine.StreamResult;
 import edsdk.api.BaseCanonCamera;
@@ -21,6 +28,7 @@ import edsdk.api.BaseCanonCamera;
  */
 public class TestLibrary  {
 
+//  @Ignore("")
   @Test
   public void testLibrary() throws Exception {
     BaseCanonCamera.initLibrary();
@@ -44,6 +52,48 @@ public class TestLibrary  {
       System.out.println("The dylib has a size of "+binlength+" bytes");
       assertEquals("Version 3.4 of EDSDK is recommended which is tested by length here",1412960,binlength);
     }
+  }
+  
+  @Test 
+  public void testLoadversion(){
+	  Assume.assumeTrue(Platform.isWindows());
+	  String filePath = "EDSDK_64\\DLL\\EDSDK.dll";
+
+      IntByReference dwDummy = new IntByReference();
+      dwDummy.setValue(0);
+
+      int versionlength =
+              com.sun.jna.platform.win32.Version.INSTANCE.GetFileVersionInfoSize(
+                      filePath, dwDummy);
+
+      byte[] bufferarray = new byte[versionlength];
+      Pointer lpData = new Memory(bufferarray.length);
+      PointerByReference lplpBuffer = new PointerByReference();
+      IntByReference puLen = new IntByReference();
+
+      boolean fileInfoResult =
+              com.sun.jna.platform.win32.Version.INSTANCE.GetFileVersionInfo(
+                      filePath, 0, versionlength, lpData);
+
+      boolean verQueryVal =
+              com.sun.jna.platform.win32.Version.INSTANCE.VerQueryValue(
+                      lpData, "\\", lplpBuffer, puLen);
+
+      VS_FIXEDFILEINFO lplpBufStructure = new VS_FIXEDFILEINFO(lplpBuffer.getValue());
+      lplpBufStructure.read();
+
+      int v1 = (lplpBufStructure.dwFileVersionMS).intValue() >> 16;
+      int v2 = (lplpBufStructure.dwFileVersionMS).intValue() & 0xffff;
+      int v3 = (lplpBufStructure.dwFileVersionLS).intValue() >> 16;
+      int v4 = (lplpBufStructure.dwFileVersionLS).intValue() & 0xffff;
+
+      String libraryversion = String.valueOf(v1) + "." +
+	          String.valueOf(v2) + "." +
+	          String.valueOf(v3) + "." +
+	          String.valueOf(v4);
+	System.out.println(
+              libraryversion);
+	assertEquals("3.5.0.6404",libraryversion);
   }
 
 }
